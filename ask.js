c1bf792391ebca85a7608256ced93ca36dbcb9ed
Alexandre DEV <calexandredev@gmail.com>
2018-07-31 11:11:19 -0300

    aspas

--- ask.js
+++ ask.js
@@ -1,14 +1,14 @@
-var questions = [
+let questions = [
   "What is your name?",
   "What is your fav hobby?",
   "What is your preferred programming language?"
 ]
 
-var answers = []
+let answers = []
 
 function ask(i) {
   process.stdout.write(`\n\n\n\n ${questions[i]}`)
-  process.stdout.write("  >  ")
+  process.stdout.write('  >  ')
 }
 
 process.stdin.on('data', function(data) {
@@ -25,11 +25,11 @@ process.stdin.on('data', function(data) {
 
 process.on('exit', function() {
 
-	process.stdout.write("\n\n\n\n")
+	process.stdout.write('\n\n\n\n')
 
 	process.stdout.write(`Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later`)
 
-	process.stdout.write("\n\n\n\n")
+	process.stdout.write('\n\n\n\n')
 
 })
 
