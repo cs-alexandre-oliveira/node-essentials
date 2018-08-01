b4d5df504e2b3b3e6eea71fa7179af610b29eabf
Alexandre DEV <calexandredev@gmail.com>
2018-08-01 18:28:57 -0300

    Reading JSON

---
+++ api.js
@@ -0,0 +1,34 @@
+let http = require('http')
+let data = require('./data/inventory')
+
+http.createServer(function(req, res)  {
+
+  if(req.url === '/'){
+    res.writeHead(200, {'Content-Type': 'text/json'})
+    res.end(JSON.stringify(data))
+  } else if(req.url === '/instock') {
+    listInStock(res)
+  } else if(req.url === '/onorder') {
+    listOnBackOrder(res)
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'})
+    res.end('ERROU ....  Data not Found')
+  }
+
+}).listen(3000)
+
+console.log('Server listening on port 3000')
+
+function listInStock(res)  {
+  let inStock = data.filter(function(item)  {
+    return item.avail ===  'In Stock'
+  })
+  res.end(JSON.stringify(inStock))
+}
+
+function listOnBackOrder(res)  {
+  let onOrder = data.filter(function(item)  {
+    return item.avail === 'On back order'
+  })
+  res.end(JSON.stringify(onOrder))
+}
