a75896ccd9eba529fde337bb6f00ebfafcda16f3
Alexandre DEV <calexandredev@gmail.com>
2018-07-31 22:32:25 -0300

    using module exports

--- benFranklin.js
+++ benFranklin.js
@@ -1,16 +1,15 @@
-let EventEmitter = require('events').EventEmitter
-let util = require('util')
-
-let Person = function(name) {
-  this.name = name
-}
-
-util.inherits(Person, EventEmitter)
+let Person = require('./lib/Person')
 
 let ben = new Person('Ben Franklin')
+let george = new Person('George Washington')
+
+george.on('speak', function(said) {
+  console.log(`${this.name}: ${said}`)
+})
 
 ben.on('speak', function(said)  {
   console.log(`${this.name}: ${said}`)
 })
 
 ben.emit('speak', 'You may nanananananana nana')
+george.emit('speak', 'I WANNA BE ....') 
