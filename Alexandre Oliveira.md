1061599c2f3d7ac9110234f82e058d58b8ea509d
Alexandre DEV <calexandredev@gmail.com>
2018-08-01 08:22:00 -0300

    Writing a file throught terminal

---
+++ Alexandre Oliveira.md
@@ -0,0 +1,5 @@
+Alexandre Oliveira
+======
+
+* Melao nao eh melhor que amarelo 
+* cachorros nao gostam de gatos, ou nao 
--- promptPerson.js
+++ promptPerson.js
@@ -1,5 +1,6 @@
 let readline = require('readline')
 let rl = readline.createInterface(process.stdin, process.stdout)
+let fs = require('fs')
 let realPerson = {
   name: '',
   sayings: []
@@ -7,12 +8,17 @@ let realPerson = {
 
 rl.question('What is the name of person? ', function(answer)  {
   realPerson.name = answer
+
+  fs.writeFileSync(realPerson.name + '.md', `${realPerson.name}\n======\n\n`)
+
   rl.setPrompt(`What would ${realPerson.name} say? `)
   rl.prompt() 
 
   rl.on('line', function(saying) {
 
     realPerson.sayings.push(saying.trim())
+
+    fs.appendFile(realPerson.name + '.md', `* ${saying.trim()} \n`)
 
     if(saying.toLowerCase().trim() === 'exit')  {
       rl.close()
