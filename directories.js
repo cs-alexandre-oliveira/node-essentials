50346b5825f2b11a4b1c1a2d8fa6951798f772ec
Alexandre DEV <calexandredev@gmail.com>
2018-08-01 09:40:22 -0300

    Removing Directorys

---
+++ directories.js
@@ -0,0 +1,25 @@
+let fs = require('fs')
+//MOVED
+//fs.renameSync('./assets/logs', './logs')
+//console.log('Directory moved')
+
+/* Working while directory is clean
+fs.rmdir('./assets', function(err)  {
+  if(err) {
+    throw err
+  }
+  console.log('Assets Directory Removed')
+})
+*/
+
+fs.readdirSync('./logs').forEach(function(fileName) {
+  fs.unlinkSync('./logs/' + fileName)
+})
+
+fs.rmdir('./logs', function(err) {
+  if(err) {
+    throw err
+  }
+  console.log('Logs Directory Removed')
+})
+
